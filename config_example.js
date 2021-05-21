31fc7ce39b7a579ea2b61ecd6a0e9122c1d6d164
stefan.balea <stefan.balea@axsys.ro>
2021-05-21 11:45:40 +0300

    added config example

---
+++ config_example.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+    discordToken: "DISCORD BOT TOKEN",
+    alphaTeamDiscordId: "MAIN TEAM DISCORD ID",
+    watchdogDiscordChannelId: 'DISCORD CHANNEL ID WHERE THE WATCHDOG WILL SEND MESSAGES',
+    allowedMinutesBetweenBlocks: 15, //minutes allowed between blocks. The watchdog will send a message and tag alpha team if the interval between two blocks is higher
+    checkIntervalInMinutes: 2, //the watchdog runs in a loop and it will check all the systems in this interval
+    averageBlockTimeRange: 20, // numbers of blocks to be fetched from current block to older blocks. this is used to compute the avg block time
+    iquidus: {
+        url: "blocks.getbze.com",
+        port: 443,
+    },
+    insight: {
+        url: "explorer.getbze.com",
+        port: 443
+    }
+}
